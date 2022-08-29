3328ec48859ff3ea02abc668ced09d51fd32502d
Robert Ball <bobball89@gmail.com>
2022-08-29 14:08:05 -0700

    updated env and seed / server .js

---
+++ seed.js
@@ -0,0 +1,28 @@
+'use strict';
+
+require('dotenv').config();
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DB_URL);
+const BookModel = require('./models/book.js');
+
+async function seed(){
+  await BookModel.create({
+    title: 'The Hobbit',
+    description: 'A hobbit goes on an unexpected journey',
+    status: 'Available'
+  });
+  await BookModel.create({
+    title: 'Green Eggs and Ham',
+    description: 'An argument about the quality of breakfast',
+    status: 'Available'
+  });
+  await BookModel.create({
+    title: 'The Prince',
+    description: 'A political treatise from 16th century Italy',
+    status: 'Available'
+  });
+  console.log('Seeded, closing connection');
+  mongoose.disconnect();
+}
+
+seed();
